8f9f896680907ba9071a96f13f2c3a2b383fee5f
IASlavnov <iaslavnov@gmail.com>
2021-02-12 19:51:53 +0300

    Добавил тестовые данные

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
-const getRandomInt = function (min, max) {
+const TYPES = ['palace', 'flat', 'house', 'bungalow'];
+const TIMES = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator'];
+const PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+const MAX_ROOMS = 5;
+const MAX_GUESTS = 10;
+const LocationCoordinates = {
+  X_MIN: 35.65,
+  X_MAX: 35.7,
+  Y_MIN: 139.7,
+  Y_MAX: 139.8,
+  precision: 5,
+};
+const ADS_COUNT = 10;
+
+const getRandomInt = (min, max) => {
   if (min < 0 || max < 0) {
     throw new Error('Диапазон значений должен быть больше или равен 0');
   }
@@ -8,7 +26,7 @@ const getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const getRandomFloat = function (min, max, decimal) {
+const getRandomFloat = (min, max, decimal) => {
   if (min < 0 || max < 0) {
     throw new Error('Диапазон значений должен быть больше или равен 0');
   }
@@ -18,10 +36,38 @@ const getRandomFloat = function (min, max, decimal) {
   return Number((Math.random() * (max - min) + min).toFixed(decimal));
 };
 
-try {
-  alert(getRandomInt(5, 10));
-  alert(getRandomFloat(10, 100, 3));
-}
-catch (Error) {
-  alert(Error.message);
-}
+const getCoordinate = (Coordinates) => {
+  return {
+    x: getRandomFloat(Coordinates.X_MIN, Coordinates.X_MAX, Coordinates.precision),
+    y: getRandomFloat(Coordinates.Y_MIN, Coordinates.Y_MAX, Coordinates.precision),
+  };
+};
+
+const getRandomArrayElement = (array) => array[getRandomInt(0, array.length - 1)];
+
+const getRandomArray = (array) => array.filter(() => Math.random() > 0.5);
+
+const createAd = () => {
+  const location = getCoordinate(LocationCoordinates);
+  return {
+    author: { avatar: 'img/avatars/user0' + getRandomInt(1, 8) + '.png' },
+    offer: {
+      title: 'Title',
+      address: location.x + ', ' + location.y,
+      price: getRandomInt(1000, Number.MAX_SAFE_INTEGER),
+      type: getRandomArrayElement(TYPES),
+      rooms: getRandomInt(1, MAX_ROOMS),
+      guests: getRandomInt(1, MAX_GUESTS),
+      checkin: getRandomArrayElement(TIMES),
+      checkout: getRandomArrayElement(TIMES),
+      features: getRandomArray(FEATURES),
+      description: 'Some description',
+      photos: getRandomArray(PHOTOS),
+    },
+    location,
+  };
+};
+
+const ads = new Array(ADS_COUNT).fill(createAd());
+
+console.log(ads);
